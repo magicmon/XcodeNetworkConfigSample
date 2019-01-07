a08897554e7742745aecb17066996734c3b6df51
magicmon <sagun25si@gmail.com>
2019-01-07 10:43:06 +0900

    display to current server url.

--- XcodeNetworkConfigSample/ViewController.swift
+++ XcodeNetworkConfigSample/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var serverLabel: UILabel!
+    @IBOutlet private var urlLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        serverLabel.text = NetworkConfig.shared.host.rawValue
+        urlLabel.text = NetworkConfig.shared.serverDomain
     }
 
     override func didReceiveMemoryWarning() {
